6a7e4c39bfd3e82e4d07ba0f9571e02b2fb6d4cb
kazekyo <k.hukusui@gmail.com>
2021-02-28 17:24:48 +0900

    Delete unnessary codes

--- example/frontend-apollo/src/App.tsx
+++ example/frontend-apollo/src/App.tsx
@@ -8,7 +8,6 @@ const APP_QUERY = gql`
     viewer {
       id
       name
-      # ...RobotList_user
     }
   }
 `;
@@ -33,21 +32,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
