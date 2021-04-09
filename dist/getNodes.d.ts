2db37d82023188713f40276a7d4615cc7278eb86
kazekyo <k.hukusui@gmail.com>
2021-04-09 19:56:09 +0900

    Delete dist directory

--- dist/getNodes.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const getNodesFromConnection: <T extends {
-    edges?: ({
-        node?: U | null | undefined;
-    } | null | undefined)[] | null | undefined;
-}, U extends unknown>({ connection, }: {
-    connection: T | null | undefined;
-}) => NonNullable<NonNullable<NonNullable<T["edges"]>[0]>["node"]>[];
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './connectionUpdater';
-export * from './getNodes';
-export * from './relayStylePagination';
-export * from './mutationUpdater';
-export * from './idAsCacheId';
