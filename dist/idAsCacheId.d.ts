d59e4e60512516cdb9022a90357b96f247dd13bb
kazekyo <k.hukusui@gmail.com>
2021-04-11 13:32:18 +0900

    Remove dist directory

--- dist/idAsCacheId.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type TypePolicies = {
-    [key: string]: {
-        [key: string]: unknown;
-    };
-};
-export declare const idAsCacheId: (typePolicies: TypePolicies, options?: {
-    idFieldName?: string | undefined;
-    excludes?: string[] | undefined;
-} | undefined) => TypePolicies;
--- dist/keyFields.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type TypePolicies = {
-    [key: string]: {
-        [key: string]: unknown;
-    };
-};
-export declare const setIdAsCacheKey: (typePolicies: TypePolicies, options?: {
-    idFieldName?: string | undefined;
-    excludes?: string[] | undefined;
-} | undefined) => TypePolicies;
