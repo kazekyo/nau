a17aec3d08ed64bac41b3631e885b5fcc07c31ed
kazekyo <k.hukusui@gmail.com>
2021-08-23 20:21:56 +0900

    Fix getNodesFromConnection generics

--- src/utils/getNodesFromConnection.ts
+++ src/utils/getNodesFromConnection.ts
@@ -1,12 +1,12 @@
-export const getNodesFromConnection = <
-  TConnection extends {
-    edges?: Array<{ node?: TNode | null } | undefined | null> | null;
-  },
-  TNode extends unknown,
->({
+export const getNodesFromConnection = <TNode>({
   connection,
 }: {
-  connection: TConnection | undefined | null;
+  connection:
+    | {
+        edges?: Array<{ node?: TNode | null } | undefined | null> | null;
+      }
+    | undefined
+    | null;
 }): TNode[] => {
   return connection?.edges?.map((edge) => edge?.node).filter((item): item is NonNullable<typeof item> => !!item) || [];
 };
