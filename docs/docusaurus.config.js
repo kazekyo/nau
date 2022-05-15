334dc286a5de5c466e8997d9cc2b6b70687d03f7
kazekyo <k.hukusui@gmail.com>
2022-05-15 18:14:04 +0900

    chore: Update docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,16 +23,13 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // TODO: Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // TODO: Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/kazekyo/nau/tree/main/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-2XPHLCBQE5',
         },
       }),
     ],
